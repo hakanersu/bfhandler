d7037b8b766d01382a138d3c171c8e0ca0a7c6f3
Hakan ERSU <hakanersu@gmail.com>
2016-04-12 11:31:23 +0300

    Mock gather method fix

--- src/Xuma/Bfhandler/Mock.php
+++ src/Xuma/Bfhandler/Mock.php
@@ -4,7 +4,7 @@ namespace Xuma\Bfhandler;
 class Mock {
     public function step()              { return $this; }
     public function execute($func)      { return $this; }
-    public function gather($type,$voice){ return $this; }
+    public function gather($ask,$error = false,$min=3,$max=10,$attempt=3){ return $this; }
     public function persist($name)      { return $this; }
     public function get($name)          { return $this; }
     public function remove($name)       { return $this; }
